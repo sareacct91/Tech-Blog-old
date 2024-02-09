f20d90835a267aa256b5b9f21057672dc089040c
Meow Roar <sareacct91@gmail.com>
2024-02-09 14:52:09 -0800

    Defined Blog model

---
+++ models/Blog.js
@@ -0,0 +1,32 @@
+const { Model, DataTypes } = require('sequelize');
+
+class Blog extends Model { };
+
+Blog.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    createdAt: true,
+    updatedAt: true,
+    modelName: 'blog',
+  }
+);
+
+module.exports = Blog;
