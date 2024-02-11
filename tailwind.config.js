a74e2d46c0bcbb2105e8cae860c4a108f825ec69
Meow Roar <sareacct91@gmail.com>
2024-02-11 00:21:14 -0800

    using custom color

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,11 @@ module.exports = {
   content: ["./views/**/*.hbs"],
   theme: {
     extend: {},
+    colors: {
+      light: "#d9ead3",
+      dark: "#231010",
+      grey: "#666666"
+    }
   },
   plugins: [],
 }
