3037c60b74f2f430768ba17a46dbfcd202516e06
Meow Roar <sareacct91@gmail.com>
2024-02-11 19:29:39 -0800

    Update Models associations with custom foreignkey and belongsToMany

--- models/index.js
+++ models/index.js
@@ -2,13 +2,16 @@ const Blog = require("./Blog");
 const Comment = require("./Comment");
 const User = require("./User");
 
-User.hasMany(Blog);
+User.hasMany(Blog, { foreignKey: { name: 'user_id', allowNull: false } });
 Blog.belongsTo(User);
+User.belongsToMany(Blog, { through: 'user_blog', uniqueKey: 'user_id' });
 
-User.hasMany(Comment);
+
+User.hasMany(Comment, { foreignKey: { name: 'user_id', allowNull: false } });
 Comment.belongsTo(User);
+User.belongsToMany(Comment, { through: 'user_comment', uniqueKey: 'user_id' });
 
-Blog.hasMany(Comment);
+Blog.hasMany(Comment, { foreignKey: { name: 'blog_id', allowNull: false } });
 Comment.belongsTo(Blog);
 
 module.exports = {
